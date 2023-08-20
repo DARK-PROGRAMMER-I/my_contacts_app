c4e5e5925794df2885cda079f5ec30d435ef9d7e
NewUser <hammadmalli64@gmail.com>
2023-08-20 22:43:43 +0500

    REST APIS

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const dotEnv = require("dotenv").config();
 
 const app = express();
+app.get('/api/contacts',(req, res)=>{
+      res.status(200).json({
+            message : "Hello I am Senior Flutter Developer",
+      });
+});
 
 const port = process.env.PORT || 5000;
 
